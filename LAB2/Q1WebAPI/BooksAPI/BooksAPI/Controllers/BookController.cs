043ec8261ca5a137c10d6572daa1d26361d97043
latuan2710 <109055879+latuan2710@users.noreply.github.com>
2024-10-16 22:31:59 +0700

    LAB2_Q1WebAPI

---
+++ LAB2/Q1WebAPI/BooksAPI/BooksAPI/Controllers/BookController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BooksAPI.Models;
+
+namespace BooksAPI.Controllers
+{
+    public class BookController : ApiController
+    {
+        private cse441_lab2_q1Entities db = new cse441_lab2_q1Entities();
+
+        // GET: api/Book
+        public IQueryable<book> Getbooks()
+        {
+            return db.books;
+        }
+
+        // GET: api/Book/5
+        [ResponseType(typeof(book))]
+        public IHttpActionResult Getbook(int id)
+        {
+            book book = db.books.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        // PUT: api/Book/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putbook(int id, book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != book.bookid)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!bookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Book
+        [ResponseType(typeof(book))]
+        public IHttpActionResult Postbook(book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.books.Add(book);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = book.bookid }, book);
+        }
+
+        // DELETE: api/Book/5
+        [ResponseType(typeof(book))]
+        public IHttpActionResult Deletebook(int id)
+        {
+            book book = db.books.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            db.books.Remove(book);
+            db.SaveChanges();
+
+            return Ok(book);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool bookExists(int id)
+        {
+            return db.books.Count(e => e.bookid == id) > 0;
+        }
+    }
+}
