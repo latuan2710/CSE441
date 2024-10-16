da2f5b291ca0e453dc6520264e742fdddf5f2d4b
latuan2710 <109055879+latuan2710@users.noreply.github.com>
2024-10-16 18:35:03 +0700

    LAB2_Q2Login

---
+++ LAB2/Q2Login/App.tsx
@@ -0,0 +1,5 @@
+import LoginScreen from "./Login/Login";
+
+export default function App() {
+  return (<LoginScreen/>);
+}
---
+++ LAB2/Q2Login/Login/Login.js
@@ -0,0 +1,27 @@
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { Styles } from './Style';
+
+export default function LoginScreen() {
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={Styles.container}>
+        <Text style={Styles.title}>Login</Text>
+        <TextInput style={Styles.input} placeholder="Phone" />
+        <TextInput
+          style={Styles.input}
+          placeholder="Password"
+          secureTextEntry
+        />
+        <TouchableOpacity style={Styles.button}>
+          <Text style={Styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+}
