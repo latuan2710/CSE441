d73d0021ce4c57ce719c7ac37d018dbede3add2c
latuan2710 <109055879+latuan2710@users.noreply.github.com>
2024-10-16 18:35:38 +0700

    LAB1_Q4ReactNative

--- LAB1/Q4reactNative/App.tsx
+++ LAB1/Q4reactNative/App.tsx
@@ -1,12 +1,11 @@
-import {ScrollView} from 'react-native';
+import { ScrollView } from 'react-native';
 import data from './Data';
 import Square from './Square';
-import { Key } from 'react';
 
-export default function App() {
+export default function App({}) {
   return (
     <ScrollView>
-      {data.map((item: Key | null | undefined, index: number) => (
+      {data.map((item: any, index: number) => (
         <Square key={item} text={`Square ${index + 1}`} />
       ))}
     </ScrollView>
